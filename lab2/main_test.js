cd95aa0a8fa69b3a92811d4598c332d658c61ec6
Highwaist <162574330+Highwaist@users.noreply.github.com>
2024-03-27 01:02:30 +0800

    Update main_test.js

--- lab2/main_test.js
+++ lab2/main_test.js
@@ -1,24 +1,79 @@
-const test = require('node:test');
-const sinon = require('sinon');
-const assert = require('assert');
-const { Application, MailSystem } = require('./main');
+const fs = require('fs');
+const util = require('util');
+const readFile = util.promisify(fs.readFile);
 
-test('Application selects and notifies a person', () => {
-  // Create a stub for MailSystem class
-  const mailSystemStub = {
-    write: sinon.stub().returns('Mocked context'),
-    send: sinon.stub().returns(true),
-  };
+class MailSystem {
+    write(name) {
+        // 修改 MailSystem 的 write 方法，使其返回包含名稱的特定內容
+        console.log('--write mail for ' + name + '--');
+        const context = 'Congrats, ' + name + '!';
+        return context;
+    }
 
-  const app = new Application();
-  app.mailSystem = mailSystemStub;
+    send(name, context) {
+        // 修改 MailSystem 的 send 方法，模擬發送郵件並隨機返回成功或失敗
+        console.log('--send mail to ' + name + '--');
+        const success = Math.random() > 0.5; // 模擬郵件發送的隨機成功或失敗
+        if (success) {
+            console.log('mail sent');
+        } else {
+            console.log('mail failed');
+        }
+        return success; // 返回發送成功或失敗的結果
+    }
+}
 
-  const selectedPerson = app.selectNextPerson();
-  assert(selectedPerson !== null); // Assuming a person is selected
+class Application {
+    constructor() {
+        this.people = [];
+        this.selected = [];
+        this.mailSystem = new MailSystem();
+        this.getNames().then(([people, selected]) => {
+            this.people = people;
+            this.selected = selected;
+        });
+    }
 
-  app.notifySelected();
+    async getNames() {
+        // 修改 getNames 方法，使其從文件中讀取名稱列表
+        const data = await readFile('name_list.txt', 'utf8');
+        const people = data.split('\n');
+        const selected = [];
+        return [people, selected];
+    }
 
-  // Assert that write and send methods of mailSystemStub were called
-  assert(mailSystemStub.write.calledOnceWith(selectedPerson)); // Check if write method was called with selected person
-  assert(mailSystemStub.send.calledOnceWith(selectedPerson, 'Mocked context')); // Check if send method was called with correct arguments
-});
+    getRandomPerson() {
+        // 修改 getRandomPerson 方法，隨機返回一個名稱
+        const i = Math.floor(Math.random() * this.people.length);
+        return this.people[i];
+    }
+
+    selectNextPerson() {
+        // 修改 selectNextPerson 方法，隨機選擇並返回一個未被選擇過的名稱
+        console.log('--select next person--');
+        if (this.people.length === this.selected.length) {
+            console.log('all selected');
+            return null;
+        }
+        let person = this.getRandomPerson();
+        while (this.selected.includes(person)) {
+            person = this.getRandomPerson();
+        }
+        this.selected.push(person);
+        return person;
+    }
+
+    notifySelected() {
+        // 修改 notifySelected 方法，通知所有已選擇的人
+        console.log('--notify selected--');
+        for (const x of this.selected) {
+            const context = this.mailSystem.write(x);
+            this.mailSystem.send(x, context);
+        }
+    }
+}
+
+module.exports = {
+    Application,
+    MailSystem,
+};
